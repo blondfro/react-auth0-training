44e4d76924228a429fdcaa3ae0967f1db0a2a39b
James Morris <blondfro@gmail.com>
2020-03-04 21:14:55 -0600

    created a login method in the auth.js. modified the app to have and pass down the auth class. added a login button to the home page.

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
 import Navigation from "./components/Navigation";
+import Auth from "./Auth/Auth";
 
-function App() {
+function App({ history }) {
+  const auth = new Auth(history);
+
   return (
     <>
       <Navigation />
       <div className="body">
-        <Route path="/" exact component={Home} />
+        <Route
+          path="/"
+          exact
+          render={props => <Home auth={auth} {...props} />}
+        />
         <Route path="/profile" component={Profile} />
       </div>
     </>
