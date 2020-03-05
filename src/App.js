3c1b3173e435f2247745c35f69b26e8bcc2cf313
James Morris <blondfro@gmail.com>
2020-03-04 18:22:30 -0600

    created a navigation component.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <>
+      <Navigation />
       <Route path="/" exact component={Home} />
       <Route path="/profile" component={Profile} />
     </>
