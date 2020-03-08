2840360887def498f84a2bfc873dfba5ed26d3a3
James Morris <blondfro@gmail.com>
2020-03-07 18:12:06 -0600

    created a way to verify the jwt and an endpoint for private data where the user must be logged in.

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const express = require("express");
 require("dotenv").config();
+const jwt = require("express-jwt"); //validates JWT and set req.user
+const jwksRsa = require("jwks-rsa"); // retrieves RSA keys from a JSON web key set (JWS) enpoint
+
+const checkJwt = jwt({
+  // Dynamically provide a signing key based on the kid in the header
+  // and the signing keys provided by the JWS endpoint.
+
+  secret: jwksRsa.expressJwtSecret({
+    cache: true, //cache the signing key
+    rateLimit: true,
+    jwksRequestsPerMinute: 5, // prevent attackers from requesting more than 5 per minute.
+    jwksUri: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/.well-known/jwks.json`
+  }),
+
+  // validate the audience and the issuer
+  audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+  issuer: `https://${process.env.REACT_APP_AUTH0_DOMAIN}/`,
+
+  /* this must match the algorithm selected in the Auth0 dashboard under your
+      app's advanced settings under the OAuth tab. 
+   */
+  algorithms: ["R5S56"]
+});
 
 const app = express();
 const PORT = 3001;
@@ -7,6 +30,12 @@ const PORT = 3001;
 app.get("/public", (req, res) => {
   res.json({
     message: "hello from a public api"
+  });
+});
+
+app.get("/private", checkJwt, (req, res) => {
+  res.json({
+    message: "hello from a private api"
   });
 });
 
