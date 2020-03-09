27849270f2739e76e4e9ba673c5510f946928eae
James Morris <blondfro@gmail.com>
2020-03-09 12:44:10 -0500

    refactored the auth to store data in memory instead of locally.

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -2,6 +2,13 @@ import auth0 from "auth0-js";
 
 const REDIRECT_ON_LOGIN = "redirect_on_login";
 
+// Stored outside class since private.
+// eslint-disable-next-line
+let _idToken = null;
+let _accessToken = null;
+let _scopes = null;
+let _expiresAt = null;
+
 export default class Auth {
   constructor(history) {
     this.history = history;
@@ -45,9 +52,7 @@ export default class Auth {
 
   setSession = authResult => {
     // set the time the session will expire.
-    const expiresAt = JSON.stringify(
-      authResult.expiresIn * 1000 + new Date().getTime()
-    );
+    _expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
 
     /*
         If there is a value on the 'scope' param from the authResult,
@@ -55,25 +60,16 @@ export default class Auth {
         use the scope as requested.  If no scopes are requested,
         set it to nothing.
      */
-    const scopes = authResult.scope || this.requestedScopes || "";
-
-    localStorage.setItem("access_token", authResult.accessToken);
-    localStorage.setItem("id_toke", authResult.idToken);
-    localStorage.setItem("expires_at", expiresAt);
-    localStorage.setItem("scopes", JSON.stringify(scopes));
+    _scopes = authResult.scope || this.requestedScopes || "";
+    _accessToken = authResult.accessToken;
+    _idToken = authResult.idToken;
   };
 
   isAuthenticated = () => {
-    const expiresAt = JSON.parse(localStorage.getItem("expires_at"));
-    return new Date().getTime() < expiresAt;
+    return new Date().getTime() < _expiresAt;
   };
 
   logout = () => {
-    localStorage.removeItem("access_token");
-    localStorage.removeItem("id_toke");
-    localStorage.removeItem("expires_at");
-    localStorage.removeItem("scopes");
-    this.userProfile = null;
     this.auth0.logout({
       clientID: process.env.REACT_APP_AUTH0_CLIENT_ID,
       returnTo: "http://localhost:3000"
@@ -81,11 +77,10 @@ export default class Auth {
   };
 
   getAccessToken = () => {
-    const accessToken = localStorage.getItem("access_token");
-    if (!accessToken) {
+    if (!_accessToken) {
       throw new Error("No access token found.");
     }
-    return accessToken;
+    return _accessToken;
   };
 
   getProfile = cb => {
@@ -97,9 +92,7 @@ export default class Auth {
   };
 
   userHasScopes(scopes) {
-    const grantedScopes = (
-      JSON.parse(localStorage.getItem("scopes")) || ""
-    ).split(" ");
+    const grantedScopes = (_scopes || "").split(" ");
     return scopes.every(scope => grantedScopes.includes(scope));
   }
 }
